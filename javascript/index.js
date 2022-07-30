b16a07af5cc4d13e586f7e388d78309a6491629a
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-07-30 22:10:59 +0200

    feat: solve iteration 2

--- javascript/index.js
+++ javascript/index.js
@@ -59,7 +59,49 @@ getInstruction(
 );
 
 // Iteration 2 - using promises
-// ...
+
+obtainInstruction("steak", 0)
+  .then(
+    (step0) =>
+      (document.querySelector("#steak").innerHTML += `<li>${step0}</li>`)
+  )
+  .then(() => obtainInstruction("steak", 1))
+  .then(
+    (step1) =>
+      (document.querySelector("#steak").innerHTML += `<li>${step1}</li>`)
+  )
+  .then(() => obtainInstruction("steak", 2))
+  .then(
+    (step2) =>
+      (document.querySelector("#steak").innerHTML += `<li>${step2}</li>`)
+  )
+  .then(() => obtainInstruction("steak", 3))
+  .then(
+    (step3) =>
+      (document.querySelector("#steak").innerHTML += `<li>${step3}</li>`)
+  )
+  .then(() => obtainInstruction("steak", 4))
+  .then(
+    (step4) =>
+      (document.querySelector("#steak").innerHTML += `<li>${step4}</li>`)
+  )
+  .then(() => obtainInstruction("steak", 5))
+  .then(
+    (step5) =>
+      (document.querySelector("#steak").innerHTML += `<li>${step5}</li>`)
+  )
+  .then(() => obtainInstruction("steak", 6))
+  .then(
+    (step6) =>
+      (document.querySelector("#steak").innerHTML += `<li>${step6}</li>`)
+  )
+  .then(() => obtainInstruction("steak", 7))
+  .then(
+    (step7) =>
+      (document.querySelector("#steak").innerHTML += `<li>${step7}</li>
+      <li>Steak is ready!</li>
+      `)
+  );
 
 // Iteration 3 using async/await
 // ...
